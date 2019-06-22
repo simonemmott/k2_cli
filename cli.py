8fcddb4c6ae7e18f114898cefd9ec87a5da91036
simon <simon.emmott@yahoo.co.uk>
2019-06-22 15:01:14 +0100

    Added basic CLI

---
+++ cli.py
@@ -0,0 +1,29 @@
+import click
+
+@click.group()
+def k2():
+    pass
+
+@click.command()
+@click.argument('application')
+def install(application):
+    click.echo('Install {application}'.format(application=application))
+    
+k2.add_command(install)
+    
+@click.group()
+def app():
+    pass
+
+k2.add_command(app)
+
+@click.command()
+def list():
+    click.echo('Application List')
+app.add_command(list)
+
+if __name__ == '__main__':
+    k2()
+
+
+    
